068a82db2c71fff42c154b23979510feb133a2b3
zhaosiy <31829627+zhaosiy@users.noreply.github.com>
2022-02-22 16:31:35 -0500

    Create module.py

---
+++ sdd/module.py
@@ -0,0 +1,187 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+
+from torch.autograd import Variable
+from utils import my_softmax, get_offdiag_indices, gumbel_softmax
+
+_EPS = 1e-10
+
+
+class MLP(nn.Module):
+    """Two-layer fully-connected ELU net with batch norm."""
+
+    def __init__(self, n_in, n_hid, n_out, do_prob=0.):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(n_in, n_hid)
+        self.fc2 = nn.Linear(n_hid, n_out)
+        self.bn = nn.BatchNorm1d(n_out)
+        self.dropout_prob = do_prob
+
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal_(m.weight.data)
+                m.bias.data.fill_(0.1)
+            elif isinstance(m, nn.BatchNorm1d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def batch_norm(self, inputs):
+        x = inputs.view(inputs.size(0) * inputs.size(1), -1)
+        x = self.bn(x)
+        return x.view(inputs.size(0), inputs.size(1), -1)
+
+    def forward(self, inputs):
+        # Input shape: [num_sims, num_things, num_features]
+        x = F.elu(self.fc1(inputs))
+        x = F.dropout(x, self.dropout_prob, training=self.training)
+        x = F.elu(self.fc2(x))
+        return self.batch_norm(x)
+
+
+class MLPEncoder(nn.Module):
+    def __init__(self, n_in, n_hid, n_out, do_prob=0., factor=True):
+        super(MLPEncoder, self).__init__()
+
+        self.factor = factor
+        self.num_message_passing = 5
+        self.mlp1 = MLP(n_in, n_hid, n_hid, do_prob)
+        self.mlp2 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+        self.mlp3 = MLP(n_hid, n_hid, n_hid, do_prob)
+        if self.factor:
+            self.mlp4 = MLP(n_hid * 3, n_hid, n_hid, do_prob)
+            print("Using factor graph MLP encoder.")
+        else:
+            self.mlp4 = MLP(n_hid * 2, n_hid, n_hid, do_prob)
+            print("Using MLP encoder.")
+        self.fc_out = nn.Linear(n_hid, n_out)
+        self.init_weights()
+
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Linear):
+                nn.init.xavier_normal(m.weight.data)
+                m.bias.data.fill_(0.1)
+
+    def edge2node(self, x, rel_rec, rel_send):
+        # NOTE: Assumes that we have the same graph across all samples.
+        # print(x.shape, rel_rec.shape, rel_rec.t().shape)
+        incoming = torch.matmul(rel_rec.t(), x)
+        return incoming / incoming.size(1)
+
+    def node2edge(self, x, rel_rec, rel_send):
+        # NOTE: Assumes that we have the same graph across all samples.
+
+        receivers = torch.matmul(rel_rec, x)
+        senders = torch.matmul(rel_send, x)
+        edges = torch.cat([senders, receivers], dim=2)
+        return edges
+
+    def forward(self, inputs, rel_rec, rel_send):
+        # Input shape: [num_sims, num_atoms, num_timesteps, num_dims] [128, 12, 100, 4]
+        x = inputs.view(inputs.size(0), inputs.size(1), -1)
+        # New shape: [num_sims, num_atoms, num_timesteps*num_dims] [128, 12, 400]
+        x = self.mlp1(x)  # 2-layer ELU net per node [128, 12, 256]
+        x = self.node2edge(x, rel_rec, rel_send)  # [128, 132, 512]
+        x = self.mlp2(x)  # [128, 132, 256]
+        x_skip = x
+
+        for _ in range(self.num_message_passing):
+            x = self.edge2node(x, rel_rec, rel_send)
+            # print(x.shape, '1')  # 128, 12, 256
+            x = self.mlp3(x)
+            # print(x.shape, '2')
+            x = self.node2edge(x, rel_rec, rel_send)
+            # print(x.shape, '3')  # 128, 132, 512
+            x = torch.cat((x, x_skip), dim=2)
+            x = self.mlp4(x)
+        x = self.edge2node(x, rel_rec, rel_send)
+        return x
+
+
+class RNNDecoder(nn.Module):
+    """Recurrent decoder module."""
+
+    def __init__(self, n_in_node, edge_types, n_hid,
+                 do_prob=0., skip_first=False):
+        super(RNNDecoder, self).__init__()
+        self.msg_fc1 = nn.ModuleList(
+            [nn.Linear(2 * n_hid, n_hid) for _ in range(edge_types)])
+        self.msg_fc2 = nn.ModuleList(
+            [nn.Linear(n_hid, n_hid) for _ in range(edge_types)])
+        self.msg_out_shape = n_hid
+        self.skip_first_edge_type = skip_first
+
+        self.hidden_r = nn.Linear(n_hid, n_hid, bias=False)
+        self.hidden_i = nn.Linear(n_hid, n_hid, bias=False)
+        self.hidden_h = nn.Linear(n_hid, n_hid, bias=False)
+
+        self.input_r = nn.Linear(n_in_node, n_hid, bias=True)
+        self.input_i = nn.Linear(n_in_node, n_hid, bias=True)
+        self.input_n = nn.Linear(n_in_node, n_hid, bias=True)
+
+        self.out_fc1 = nn.Linear(n_hid, n_hid)
+        self.out_fc2 = nn.Linear(n_hid, n_hid)
+        self.out_fc3 = nn.Linear(n_hid, n_in_node)
+
+        print('Using learned recurrent interaction net decoder.')
+
+        self.dropout_prob = do_prob
+
+    def single_step_forward(self, inputs, rel_rec, rel_send, hidden):
+
+        # node2edge
+        receivers = torch.matmul(rel_rec, hidden)
+        senders = torch.matmul(rel_send, hidden)
+        pre_msg = torch.cat([senders, receivers], dim=-1)
+        # print(pre_msg.shape,'premsg') [128, 132, 512]
+        all_msgs = Variable(torch.zeros(pre_msg.size(0), pre_msg.size(1),
+                                        self.msg_out_shape))
+        # print(all_msgs.shape,'all msg') [128, 132, 256]
+        if inputs.is_cuda:
+            all_msgs = all_msgs.cuda()
+
+        agg_msgs = all_msgs.transpose(-2, -1).matmul(rel_rec).transpose(-2,
+                                                                        -1)
+        agg_msgs = agg_msgs.contiguous() / inputs.size(2)  # Average
+
+        # GRU-style gated aggregation
+        r = F.sigmoid(self.input_r(inputs) + self.hidden_r(agg_msgs))
+        i = F.sigmoid(self.input_i(inputs) + self.hidden_i(agg_msgs))
+        n = F.tanh(self.input_n(inputs) + r * self.hidden_h(agg_msgs))
+        hidden = (1 - i) * n + i * hidden
+
+        # Output MLP
+        pred = F.dropout(F.relu(self.out_fc1(hidden)), p=self.dropout_prob)
+        pred = F.dropout(F.relu(self.out_fc2(pred)), p=self.dropout_prob)
+        pred = self.out_fc3(pred)
+        # Predict position/velocity difference
+        pred = inputs + pred
+
+        return pred, hidden
+
+    def forward(self, encoderr,inputs, timestep, rel_rec, rel_send, pred_steps=1,
+                burn_in=False, burn_in_steps=1, dynamic_graph=False,
+                encoder=None, temp=None):
+        inputs = inputs.transpose(1, 2).contiguous()[:,:,:,:2]
+        pred_all = []
+        ins = inputs[:, 0, :, :]
+        for step in range(0, 19):
+
+            if step > 0:
+                #ins = pred_all[step - 1]
+                 ins = inputs[:, len(pred_all), :, :]
+            updated_nodes = encoderr(ins, rel_rec, rel_send)
+            hidden = updated_nodes
+            pred, hidden = self.single_step_forward(ins, rel_rec, rel_send, hidden)
+            pred_all.append(pred)
+
+        preds = torch.stack(pred_all, dim=1)
+
+        return preds.transpose(1, 2).contiguous()
+~                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+~                                                                                                                                                                                                              
